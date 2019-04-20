fbef1a85cf8ebf33fff86f9aa8cb27791f07de80
Dasha Metropolitansky <metropolitansky@college.harvard.edu>
2019-04-20 14:00:26 -0400

    Created animation

--- shotspotter.R
+++ shotspotter.R
@@ -5,6 +5,9 @@ library(readr)
 library(fs)
 library(sf)
 library(lubridate)
+library(gganimate)
+library(transformr)
+library(ggthemes)
 
 fresno = read_csv("http://justicetechlab.org/wp-content/uploads/2018/09/fresno_sst.csv", col_types = cols(
   address = col_character(),
@@ -31,14 +34,32 @@ fresno_final = fresno %>%
   filter(!is.na(long)) %>%
   filter(long > -120 & long < -119.45) %>%
   filter(lat > 36.5 & lat < 37) %>%
-  mutate(day = date(datetime)) %>%
-  mutate(day = format(day, format = "%B %e, %Y"))
+  mutate(date_shot = date(datetime)) %>%
+  arrange(date_shot) 
 
 locations = st_as_sf(fresno_final, coords = c("long", "lat"), crs = 4326)
 
 ggplot(data = shapes) + 
   geom_sf() +
-  geom_sf(data = locations, aes(alpha = 0.5, size = numrounds))
+  geom_sf(data = locations, aes(colour = numrounds, alpha = 0.6)) + 
+  labs(caption = "Source: Justice Tech Lab ShotSpotter Data") +
+  guides(alpha = FALSE) +
+  scale_colour_gradient(name = "Rounds Fired", 
+                        low = "mediumblue", 
+                        high = "orangered3") + 
+  theme(legend.position = c(0.8, 0.1)) +
+  theme_map() +
+  theme(panel.grid.major = element_line(colour = "white")) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  #Colouring the map gridlines white
+  #Making each data a different frame in the animation
+  transition_time(date_shot) + 
+  ease_aes() +
+  #Leaving each previous frame as a permanent mark on the map
+  shadow_mark(past = TRUE) +
+  #Adding a title that includes the date shown in the present frame
+  ggtitle("Gunshots Fired in Fresco, California on {frame_time}")
+  
 
 
 
